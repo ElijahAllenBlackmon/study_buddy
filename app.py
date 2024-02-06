9862e30e3e76192c7e1d1785eada8afbab6805cd
Elijah Blackmon <elijahblackmon1010@gmail.com>
2024-02-06 05:14:23 -0600

    changes

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
-print("Hello Python World!")
-print("Hello Python World..Again")
+yes = True
+no = False
+question = 1
+score = 0
+
+key = {
+    input('What\'s the name for a "special" port on a router? '): "interface",
+}
+
+
+while yes:
+    print("Question", question)
+    for x,y in key.items():
+        x
+        if x == y:
+            print("Correct")
+            score += 1
+        else:
+            print("Incorrect")
+        question += 1
+    yes = False
+
+
+# for x, y in ch1_key.items():
+#     print("Question.", x,y)
