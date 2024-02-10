65781ac0b7aba2c4831f6c2c60d50fed3e0dbdad
Elijah Blackmon <elijahblackmon1010@gmail.com>
2024-02-10 16:39:37 -0600

    added data

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ print("Chapter 1 Questions\n")
 def main():
     CH1_score = 0
     CH3_score = 0
-    # CH2_score = 0
+    CH2_score = 0
     # CH4_score = 0
+
 
     for x in range(len(data.CH1_Q)):
 
@@ -19,6 +20,7 @@ def main():
             print()
             print(data.CH1_Q[x])
             print("Incorrect\n\n")
+
     for x in range(len(data.CH3_Q)):
         if data.CH3_A[x] == data.CH3_Q[x]:
             print()
@@ -30,8 +32,20 @@ def main():
             print(data.CH3_Q[x])
             print("Incorrect\n\n")
 
+    for x in range(len(data.CH2_Q)):
+        if data.CH2_A[x] == data.CH2_Q[x]:
+            print()
+            print(data.CH2_Q[x])
+            print("Correct\n\n")
+            CH2_score += 1
+        else:
+            print()
+            print(data.CH2_Q[x])
+            print("Incorrect\n\n")
+
     print(f"Chapter 1 Final Score: {CH1_score}/{len(data.CH1_Q)}\n")
     print(f"Chapter 3 Final Score: {CH3_score}/{len(data.CH3_Q)}\n")
+    print(f"Chapter 2 Final Score: {CH2_score}/{len(data.CH2_Q)}\n")
 
 
 main()
