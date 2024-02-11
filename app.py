a76d9e3d9db152286bf401b2648df7644af7092d
Elijah Blackmon <elijahblackmon1010@gmail.com>
2024-02-11 04:24:45 -0600

    chapter 4 implementation

--- app.py
+++ app.py
@@ -1,55 +1,56 @@
 import data
 
 def main():
-    CH1_score = 0
-    CH3_score = 0
-    CH2_score = 0
-    # CH4_score = 0
+    # CH1_score = 0
+    # CH3_score = 0
+    # CH2_score = 0
+    CH4_score = 0
 
     # Prompts Chapter 1 Questions
-    for x in range(len(data.CH1_Q)):
+    # for x in range(len(data.CH1_Q)):
 
-        if data.CH1_A[x] == data.CH1_Q[x]:
-            print()
-            print(data.CH1_Q[x])
-            print("Correct\n\n")
-            # Increases score for chapter
-            CH1_score += 1
-        else:
-            print()
-            print(data.CH1_Q[x])
-            print("Incorrect\n\n")
+    #     if data.CH1_A[x] == data.CH1_Q[x]:
+    #         print()
+    #         print(data.CH1_Q[x])
+    #         print("Correct\n\n")
+    #         # Increases score for chapter
+    #         CH1_score += 1
+    #     else:
+    #         print()
+    #         print(data.CH1_Q[x])
+    #         print("Incorrect\n\n")
 
     # Prompts Chapter 3 Questions
-    for x in range(len(data.CH3_Q)):
-        if data.CH3_A[x] == data.CH3_Q[x]:
-            print()
-            print(data.CH3_Q[x])
-            print("Correct\n\n")
-            # Increases score for chapter
-            CH3_score += 1
-        else:
-            print()
-            print(data.CH3_Q[x])
-            print("Incorrect\n\n")
+    # for x in range(len(data.CH3_Q)):
+    #     if data.CH3_A[x] == data.CH3_Q[x]:
+    #         print()
+    #         print(data.CH3_Q[x])
+    #         print("Correct\n\n")
+    #         # Increases score for chapter
+    #         CH3_score += 1
+    #     else:
+    #         print()
+    #         print(data.CH3_Q[x])
+    #         print("Incorrect\n\n")
 
     # Prompts Chapter 2 Questions
-    for x in range(len(data.CH2_Q)):
-        if data.CH2_A[x] == data.CH2_Q[x]:
-            print()
-            print(data.CH2_Q[x])
-            print("Correct\n\n")
-            # Increases score for chapter
-            CH2_score += 1
-        else:
-            print()
-            print(data.CH2_Q[x])
-            print("Incorrect\n\n")
+    # for x in range(len(data.CH2_Q)):
+    #     if data.CH2_A[x] == data.CH2_Q[x]:
+    #         print()
+    #         print(data.CH2_Q[x])
+    #         print("Correct\n\n")
+    #         # Increases score for chapter
+    #         CH2_score += 1
+    #     else:
+    #         print()
+    #         print(data.CH2_Q[x])
+    #         print("Incorrect\n\n")
 
     # Prints final score of each chapter
-    print(f"Chapter 1 Final Score: {CH1_score}/{len(data.CH1_Q)}\n")
-    print(f"Chapter 3 Final Score: {CH3_score}/{len(data.CH3_Q)}\n")
-    print(f"Chapter 2 Final Score: {CH2_score}/{len(data.CH2_Q)}\n")
+    # print(f"Chapter 1 Final Score: {CH1_score}/{len(data.CH1_Q)}\n")
+    # print(f"Chapter 3 Final Score: {CH3_score}/{len(data.CH3_Q)}\n")
+    # print(f"Chapter 2 Final Score: {CH2_score}/{len(data.CH2_Q)}\n")
+    print(f"Chapter 2 Final Score: {CH4_score}/{len(data.CH4_Q)}\n")
 
 
 main()
